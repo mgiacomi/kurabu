fa404f997945bb4ea6e8e7b931ae9345c3114536
mgiacomi <Rebeka13!>
2017-03-15 23:41:08 -0700

    change to 2017

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -6,7 +6,8 @@ class Signup < ActiveRecord::Base
   validates :cday, :cmonth, :cyear, :cage, presence: {message: " is required"}
   validates :p1fname, :p1lname, :street, :city, :state, :zip, :email, :phone1, presence: {message: " is required"}
   validates :econtact_name, :econtact_relationship, :econtact_phone, :econtact_address, presence: {message: " is required"}
-  validates :activity_cname, :activity_agree, :activity_signed, :activity_signed_date, presence: {message: " is required"}
+  validates :activity_cname, :activity_signed, :activity_signed_date, presence: {message: " is required"}
+  validates :activity_agree, :presence => true, :inclusion => { :in => ['Yes'], message: " must be checked" }
   validate :at_least_one_session
 
   class << self
