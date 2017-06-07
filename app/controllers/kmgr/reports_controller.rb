c4bde3cf8baff601797b48ab35826dd1613b8d52
mgiacomi <Rebeka13!>
2017-06-06 17:03:11 -0700

    Add sign in sheets

--- app/controllers/kmgr/reports_controller.rb
+++ app/controllers/kmgr/reports_controller.rb
@@ -115,9 +115,7 @@ class Kmgr::ReportsController < ApplicationController
   def signupsheet
     # Service authentication to Google
     scopes = [Google::Apis::SheetsV4::AUTH_SPREADSHEETS, Google::Apis::SheetsV4::AUTH_DRIVE]
-#    authorization = Google::Auth.get_application_default(scopes)
-#    jason_key = CredentialsLoader::makeCredentials($scope, json_decode(getenv('GOOGLE_CREDENTIALS'), true))
-    authorization = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: getenv('KURABU_KEY'), scope: scopes)
+    authorization = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: ENV['KURABU_KEY'], scope: scopes)
 
     # Initialize Sheets API
     s_service = Google::Apis::SheetsV4::SheetsService.new
