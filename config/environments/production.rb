357c000a77527e5d813a8eca643e6757894885f7
mgiacomi <mgiacomi@gltech.com>
2022-02-27 16:51:53 -0800

    Upgrade to Rails 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,15 +4,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'kurabu.herokuapp.com' }
 
-  ActionMailer::Base.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'heroku.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+  #ActionMailer::Base.smtp_settings = {
+  #    :user_name => ENV['SENDGRID_USERNAME'],
+  #    :password => ENV['SENDGRID_PASSWORD'],
+  #    :domain => 'heroku.com',
+  #    :address => 'smtp.sendgrid.net',
+  #    :port => 587,
+  #    :authentication => :plain,
+  #    :enable_starttls_auto => true
+  #}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
